6125928cd7e3306b4d3f764703d8297c125f65cc
Breno Castelo Branco <breno@vizir.com.br>
2020-05-27 20:36:07 -0300

    feat: add sign up screen

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,12 +1,69 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useRef } from 'react';
+import { Image } from 'react-native';
+import PropTypes from 'prop-types';
 
+import logo from '~/assets/logo.png';
 import Background from '~/components/Background';
 
-export default function SignUp() {
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignUp({ navigation }) {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  function handleSubmit() {}
+
   return (
     <Background>
-      <Text>SignUp</Text>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect
+            autoCaptilize
+            placeholder="Type your fullname"
+            returnKeyType="next"
+            onSubmitEditing={() => emailRef.current.focus()}
+          />
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCaptilize={false}
+            placeholder="Type your e-mail"
+            ref={emailRef}
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Type your password"
+            returnKeyType="send"
+            ref={passwordRef}
+            onSubmitEditing={handleSubmit}
+          />
+          <SubmitButton onPress={handleSubmit}>Sign Up</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignLinkText>I have account</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
+
+SignUp.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
+};
